9a13e8d77695ffb43a5bd11af1ac6983d37470b6
David Grimm <dtg58@scarletmail.rutgers.edu>
2018-12-06 16:01:15 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-  // put setup code here
+  createCanvas(710, 400);
+  background(102);/ put setup code here
 }
 
 function draw() {
-  // put drawing code here
+  variableEllipse(mouseX, mouseY, pmouseX, pmouseY);/ put drawing code here
+}
+
+function variableEllipse(x, y, px, py) {
+  var speed = abs(x-px) + abs(y-py);
+  stroke(speed);
+  ellipse(x, y, speed, speed);
 }
