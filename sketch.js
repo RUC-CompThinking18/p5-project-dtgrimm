0ea3efc891c798ea6a0bb2f5f509e4f6cf06185d
David Grimm <dtg58@scarletmail.rutgers.edu>
2018-12-11 13:20:51 -0500

    p5
    moving image

--- sketch.js
+++ sketch.js
@@ -1,14 +1,42 @@
+// This program simply allows the user to move around an image Nicolas Cage.
+//It loads a background image, then the interactive image. The interactive image has a transparent effect put on it.
+// He is in space for he makes up all living things.
+
+var sound;
+var img;
+var offset = 0;
+var easing = 0.05//moves the image closer to the cursor if the distance is more than one pixel.
+
+function preload(){
+  sound = loadSound('lordquotes.mp3');
+}
+
+
+
+function preload(){
+  img = loadImage('tadpolegalaxy.jpg');//loads the image to use as the background
+}
+
 function setup() {
-  createCanvas(710, 400);
-  background(102);/ put setup code here
+  createCanvas(1738, 790);//sets dimensions of background image
+  image(img, 0, 0)//sets postion of background image
+  loadPixels();//allows the image to actually appear
+  img = loadImage('onetruegod.jpg');//loads the interactive image
+
 }
 
 function draw() {
-  variableEllipse(mouseX, mouseY, pmouseX, pmouseY);/ put drawing code here
+  image(img, 0, 100);//
+  var dx = (mouseX-img.width/2) - offset;//allows the image to follow the cursor
+  offset += dx * easing;
+  tint(75, 50);//sets the opacity of the interactive image
+  image(img, offset, 100);
 }
 
-function variableEllipse(x, y, px, py) {
-  var speed = abs(x-px) + abs(y-py);
-  stroke(speed);
-  ellipse(x, y, speed, speed);
+function mousePressed() {
+  if ( sound.isPlaying() ) {
+    sound.stop();
+  } else {
+    sound.play();
+  }
 }
