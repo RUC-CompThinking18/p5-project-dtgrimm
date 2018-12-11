401d2490b09da3f9a6d6e21f40f07d4cac203b07
David Grimm <dtg58@scarletmail.rutgers.edu>
2018-12-11 13:34:58 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
-// This program simply allows the user to move around an image Nicolas Cage.
-//It loads a background image, then the interactive image. The interactive image has a transparent effect put on it.
-// He is in space for he makes up all living things.
+/* This program simply allows the user to move around an image Nicolas Cage.
+It loads a background image, then the interactive image. The interactive image has a transparent effect put on it.
+He is in space for he makes up all living things.
+I was having a hard time coming up with a good idea for this project, so I decided to make something that is vaguely entertaining while still containing interactive elements for the user to use.
+ */
 
 var sound;
 var img;
-var offset = 0;
+var offset = 0;//sets image postion
 var easing = 0.05//moves the image closer to the cursor if the distance is more than one pixel.
-
-
 
 
 function preload(){
   img = loadImage('tadpolegalaxy.jpg');//loads the image to use as the background
-  sound = loadSound('lordquotes.mp3');
+  sound = loadSound('lordquotes.mp3');//loads mp3 file to be utilized later
 }
 
 function setup() {
@@ -24,16 +24,16 @@ function setup() {
 }
 
 function draw() {
-  image(img, 0, 100);//
+  image(img, 0, 100);
   var dx = (mouseX-img.width/2) - offset;//allows the image to follow the cursor
   offset += dx * easing;
   tint(75, 50);//sets the opacity of the interactive image
-  image(img, offset, 100);
+  image(img, offset, 100);// the "100" makes the Nicolas Cage picture more center
 }
 
-function mousePressed() {
+function mousePressed() {//plays series of Nicolas Cage quotes when the mouse is pressed.
   if ( sound.isPlaying() ) {
-    sound.stop();
+    sound.stop();//stops sound when mouse is pressed
   } else {
     sound.play();
   }
